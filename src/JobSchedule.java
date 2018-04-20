3c4d42d0548de28e9a34d8ec34fae6896745cd6f
Isabelle Delmas <isabelle.lenertz@gmail.com>
2018-04-19 22:25:46 -0700

    test 1 in blue, test 2 in green

--- src/JobSchedule.java
+++ src/JobSchedule.java
@@ -1,32 +1,23 @@
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 
 public class JobSchedule {
 	private ArrayList<Job> list;
-	private ArrayList<Job> rTopo;
 	
 	public class Job{
 		private int time;
-		private ArrayList<Job> incomingList;
 		private ArrayList<Job> outgoingList;
 		private int d;
-		private JobSchedule schedule;
-		private boolean discovered;	
 		private int inDegree;
-		private static final int START_NOT_SET = -1;
 
 		
 		/**
 		 * Construct a job with a defined time
 		 * @param t time needed to complete the job
 		 */
-		protected Job(int t, JobSchedule thisSchedule) {
-			incomingList = new ArrayList<Job>();
+		protected Job(int t) {
 			outgoingList = new ArrayList<Job>();
 			time = t;
-			schedule = thisSchedule;
 		}
 		
 		/**
@@ -35,7 +26,6 @@ public class JobSchedule {
 		 */
 		public void requires(Job j) {
 			// Add j to the incoming list of this
-			incomingList.add(j);
 			j.outgoingList.add(this);
 		}
 		
@@ -44,94 +34,62 @@ public class JobSchedule {
 		 * @return
 		 * @throws Exception if there is a cycle or if the job cannot be reached
 		 */
-		public int getStartTime() {
-			// Initialize the list for DFS
-			schedule.initializeDFS();
-						
-
-			return this.d;
-			
-		}
-		
-		/**
-		 * Relax edge u->v, where this is v
-		 * @param u incoming job, u.time is the weight of the edge
-		 * @throws Exception 
-		 */
-		/**private void relax(Job u) throws Exception {
-			if(u.d + u.time > this.d || this.d == Integer.MAX_VALUE) {
-				if (this.isFinished()) {
-					throw new Exception("No solution");
-				}
-				this.d = u.d + u.time;
-				this.parent = u;
+		public int getStartTime() {				
+			// Initializes the in degree to 0
+			for(Job u : list) {
+				u.inDegree = 0;
 			}
-		}/*
-		
-
-		
-		/**
-		 * Get the jobs in reverse topological order
-		 * @return
-		 */
-		private void getRTopological(ArrayList<Job> rTopo){
-			this.discovered = true;
-			
-			for(Job element : incomingList) {
-				if(element.discovered == false) {
-					element.getRTopological(rTopo);
+			// Get the actual inDegree for each job
+			for(Job u : list) {
+				for(Job v : u.outgoingList) {
+					v.inDegree++;				
+				}
+				
+			}
+			// Put all the Job with no requirement in the list
+			List<Job> topoList = new ArrayList<Job>();
+			for(Job u : list) {
+				if(u.inDegree == 0) {
+					u.d = 0;
+					topoList.add(u);
+				} else {
+					u.d = -1;
 				}
 			}
-			rTopo.add(this);
+			// Decrease in degree of all the jobs in the list
+			// Add the job that have now in degree 0
+			int i = 0;
+			while(i < topoList.size()) {
+				Job u = topoList.get(i);
+				for(Job v : u.outgoingList) {
+					v.inDegree--; 
+					v.d = Integer.max(v.d, u.d + u.time);
+					if(v.inDegree == 0)
+						if(v == this) {
+							return v.d;
+						}
+						topoList.add(v);
+					}
+			}		
+			// if this was never reached
+			return -1;		
 		}
-		
 		
 	}
 	
 	
 	public JobSchedule() {
 		list = new ArrayList<Job>();
-		rTopo = new ArrayList<Job>();
 	}
-	
-	/**
-	 * Set all the flags to false
-	 */
-	private void initializeDFS() {
-		for(Job element : list) {
-			if (element.incomingList.isEmpty()) {
-				element.d = 0;
-			} else {
-				element.d = -1;
-			}
-		}
-	}
-	
-	/**
-	 * Get the list of jobs in reverse topological order
-	 * using algo in video "Topological Order and Topological sorting, Taylor, 4:09)
-	 * @return the list of jobs in reverse topological order
-	 */
-	private ArrayList<Job> getReverseTopo() {
-		this.initializeDFS();
-		ArrayList<Job> rTopo = new ArrayList<Job>();
-		for(Job element : list) {
-			if(element.discovered == false) {
-				element.getRTopological(rTopo);
-			}
-		}
-		return rTopo;
-	}
-	
+		
 	/**
 	 * Add a new job to the schedule
 	 * @param time time to complete the job
 	 * @return a reference to the job that was created
 	 */
 	public Job addJob(int time) {
-		Job j = new Job(time, this);
+		Job j = new Job(time);
 		list.add(j);
-		rTopo = this.getReverseTopo();
 		return j;
 	}
 	
